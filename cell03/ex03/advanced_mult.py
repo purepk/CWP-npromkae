a1c67e7cda373b2b17abfd445b46d2d3bb85f44b
purepk <nutpong.pk@gmail.com>
2026-09-22 20:01:54 +0700

    ex303

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,15 @@
+import sys
+def main() :
+    if len(sys.argv) > 1 :
+        print("none")
+    else :
+        i = 0
+        while i <= 10 :
+            j = 0
+            list_num = []
+            while j <= 10 :
+                list_num.append(str(i * j))
+                j += 1
+            print(f"Table de {i} : {' '.join(list_num)}")
+            i += 1
+main()
