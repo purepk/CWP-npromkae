eb66d4e9225b51587432158beb312dd47c56aa32
purepk <nutpong.pk@gmail.com>
2026-09-22 19:59:36 +0700

    ex300

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,10 @@
+def main() :
+    print("Enter a number less than 25")
+    num = int(input())
+    if num > 25 :
+        print("Error")
+    else :
+        while num <= 25 :
+            print(f"Inside the loop, my variable is {num}")
+            num += 1
+main()
