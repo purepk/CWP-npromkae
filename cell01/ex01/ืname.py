f707b9c1790864a9aea76b7103c82400a3b6672a
purepk <nutpong.pk@gmail.com>
2026-09-22 19:51:24 +0700

    ex102

--- cell01/ex01/ืname.py
+++ cell01/ex01/ืname.py
@@ -2,5 +2,4 @@ first_name = "nutpong"
 last_name = "promkaew"
 whole_name = first_name+" "+last_name
 
-
 print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age = int(input())
+intra = 42
+my_age = age + intra
+print(my_age)
